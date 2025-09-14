6529cf1adcbdc9e26a0f60ee2ec4de616916730a
Jekabs Leinis <jekabsleinis@gmail.com>
2025-09-14 22:25:29 +0300

    Make IsLatvian non-nullable

---
+++ BalsisNoteSheetLibrary.Server/Migrations/20250914191531_MakeIsLatvianNonNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BalsisNoteSheetLibrary.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeIsLatvianNonNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsLatvian",
+                table: "NoteSheets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsLatvian",
+                table: "NoteSheets",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+    }
+}

43922f3df20c07378dc412472956de3f47260c38
Jekabs Leinis <jekabsleinis@gmail.com>
2026-02-08 19:35:44 +0200

    Refactor vLoading.js to use Vue lifecycle hooks for loading state management

--- balsisnotesheetlibrary.client/src/directives/vLoading.js
+++ balsisnotesheetlibrary.client/src/directives/vLoading.js
@@ -1,5 +1,17 @@
-// From https://codesandbox.io/p/sandbox/nostalgic-noyce-18306n50rj?file=%2Fsrc%2FApp.vue%3A10%2C9
-export default function (el, binding) {
+// Modified from https://codesandbox.io/p/sandbox/nostalgic-noyce-18306n50rj?file=%2Fsrc%2FApp.vue%3A10%2C9
+export default {
+  mounted(el, binding) {
+    updateLoadingState(el, binding);
+  },
+  updated(el, binding) {
+    updateLoadingState(el, binding);
+  },
+  unmounted(el) {
+    updateLoadingState(el, { value: false });
+  }
+};
+
+function updateLoadingState(el, binding) {
   if (binding.value) {
     el.classList.add("state-loading");
     if (binding.modifiers.bg) {
